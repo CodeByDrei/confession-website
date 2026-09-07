47c98661982c1f7c91d2253c036f498bdca39055
CodeByDrei <janofthezafra@gmail.com>
2026-09-07 18:36:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-const button = document.querySelector("#openButton");
-
+const openButton = document.querySelector("#openButton");
 const intro = document.querySelector("#intro");
-
 const letter = document.querySelector("#letter");
 
 const envelope = document.querySelector("#envelope");
 
+const continueButton = document.querySelector("#continueButton");
+const question = document.querySelector("#question");
 
-/* =========================
-   OPEN FIRST SCREEN
-========================= */
 
-button.addEventListener("click", function() {
+// =========================
+// INTRO → LETTER
+// =========================
+
+openButton.addEventListener("click", function () {
 
     intro.classList.add("fade-out");
 
-
-    setTimeout(function() {
+    setTimeout(function () {
 
         intro.classList.add("hidden");
 
         letter.classList.remove("hidden");
-
         letter.classList.add("fade-in");
 
     }, 500);
@@ -29,21 +28,33 @@ button.addEventListener("click", function() {
 });
 
 
-/* =========================
-   OPEN ENVELOPE
-========================= */
+// =========================
+// OPEN ENVELOPE
+// =========================
 
-envelope.addEventListener("click", function() {
+envelope.addEventListener("click", function () {
 
-    /* Prevent clicking again */
-    if (envelope.classList.contains("open")) {
-        return;
-    }
-
-
-    /* Open envelope */
     envelope.classList.add("open");
-
     letter.classList.add("opened");
+
+});
+
+
+// =========================
+// LETTER → QUESTION
+// =========================
+
+continueButton.addEventListener("click", function () {
+
+    letter.classList.add("fade-out");
+
+    setTimeout(function () {
+
+        letter.classList.add("hidden");
+
+        question.classList.remove("hidden");
+        question.classList.add("fade-in");
+
+    }, 500);
 
 });
