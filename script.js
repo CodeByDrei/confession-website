7adf9fde0c3c8c690e27165bd66ee0dd46459346
CodeByDrei <janofthezafra@gmail.com>
2026-09-07 17:10:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const button = document.querySelector("#openButton");
+
 const intro = document.querySelector("#intro");
+
 const letter = document.querySelector("#letter");
 
+
 button.addEventListener("click", function() {
+
     intro.classList.add("fade-out");
 
+
     setTimeout(function() {
+
         intro.classList.add("hidden");
+
         letter.classList.remove("hidden");
+
         letter.classList.add("fade-in");
+
     }, 500);
+
 });
